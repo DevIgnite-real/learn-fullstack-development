abc8adc4632e72978193bc6ffcea61a92b0fc1e9
Yorsua Alfonso <227312215+StackShadow-tech@users.noreply.github.com>
2026-05-01 18:10:41 -0400

    string exercises completed

--- 03. JavaScript Fundamentals/01. Build a Counter App/18. Write your first string variable/Exercise 1/index.js
+++ 03. JavaScript Fundamentals/01. Build a Counter App/18. Write your first string variable/Exercise 1/index.js
@@ -1,6 +1,9 @@
 let username = "per"
-
 // Create a variable, message, that stores the string: "You have tree new notifications"
 
-console.log(username)
+let message = "You have tree new notifications"
+
+let messageToUser = message + ", " + username + "!"
+
+console.log(messageToUser)
 
