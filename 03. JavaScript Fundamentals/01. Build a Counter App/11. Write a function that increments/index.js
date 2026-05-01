859decc922b2aeb6f4030ce3d559094e04860db4
Yorsua Alfonso <227312215+StackShadow-tech@users.noreply.github.com>
2026-05-01 17:11:35 -0400

    javascript challenge completed

--- 03. JavaScript Fundamentals/01. Build a Counter App/11. Write a function that increments/index.js
+++ 03. JavaScript Fundamentals/01. Build a Counter App/11. Write a function that increments/index.js
@@ -3,7 +3,17 @@ let lapsCompleted = 0
 // Create a function that increments the lapsCompleted variable with one
 // Run it three times
 
+function logtotallaps() {
+    lapsCompleted = lapsCompleted + 1
+    console.log(lapsCompleted)
 
+    lapsCompleted = lapsCompleted + 1
+    console.log(lapsCompleted)
+
+    lapsCompleted = lapsCompleted + 1
+}
+
+logtotallaps()
 
 
 console.log(lapsCompleted)
