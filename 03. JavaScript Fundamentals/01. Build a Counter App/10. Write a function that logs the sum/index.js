03a4bd1ae3b74ba4222de45f91b9c301ed0837ab
Yorsua Alfonso <227312215+StackShadow-tech@users.noreply.github.com>
2026-05-01 16:21:51 -0400

    function challenge completed

--- 03. JavaScript Fundamentals/01. Build a Counter App/10. Write a function that logs the sum/index.js
+++ 03. JavaScript Fundamentals/01. Build a Counter App/10. Write a function that logs the sum/index.js
@@ -4,3 +4,10 @@ let lap3 = 36
 
 // Create a function that logs out the sum of all the lap times
 
+function sumlaps() {
+    let sumoflaps = lap1 + lap2 + lap3
+    console.log(sumoflaps)
+}
+
+
+// sumlaps()
