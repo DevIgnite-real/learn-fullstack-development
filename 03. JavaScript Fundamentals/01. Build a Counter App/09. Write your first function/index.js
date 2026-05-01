b29a31521fb1ce29708208af4043275c7f622e2b
Yorsua Alfonso <227312215+StackShadow-tech@users.noreply.github.com>
2026-05-01 15:38:49 -0400

    written my first solo function

--- 03. JavaScript Fundamentals/01. Build a Counter App/09. Write your first function/index.js
+++ 03. JavaScript Fundamentals/01. Build a Counter App/09. Write your first function/index.js
@@ -1,3 +1,9 @@
 // Create a function (you decide the name) that logs out the number 42 to the console
 // Call/invoke the function
 
+function invokenum() {
+    let num = 42
+    console.log(num)
+}
+
+invokenum()
