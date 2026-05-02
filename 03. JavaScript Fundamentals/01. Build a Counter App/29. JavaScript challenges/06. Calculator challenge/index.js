ad64ce09767746b1ec91237974244b98b5d2f11e
Yorsua Alfonso <227312215+StackShadow-tech@users.noreply.github.com>
2026-05-02 19:59:10 -0400

    challenge completed

--- 03. JavaScript Fundamentals/01. Build a Counter App/29. JavaScript challenges/06. Calculator challenge/index.js
+++ 03. JavaScript Fundamentals/01. Build a Counter App/29. JavaScript challenges/06. Calculator challenge/index.js
@@ -3,6 +3,8 @@ let num2 = 2
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
 
+let sumEl = document.getElementById("sum-el")
+
 // Create four functions: add(), subtract(), divide(), multiply()
 // Call the correct function when the user clicks on one of the buttons
 // Perform the given calculation using num1 and num2
@@ -12,4 +14,22 @@ document.getElementById("num2-el").textContent = num2
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
 
 
+function add() {
+    let result = num1 + num2
+    sumEl.textContent += " " + result
+}
 
+function subtract() {
+    let result = num1 - num2
+    sumEl.textContent += " " + result
+}
+
+function divide() {
+    let result = num1 / num2
+    sumEl.textContent += " " + result
+}
+
+function multiply() {
+    let result = num1 * num2
+    sumEl.textContent += " " + result
+}
