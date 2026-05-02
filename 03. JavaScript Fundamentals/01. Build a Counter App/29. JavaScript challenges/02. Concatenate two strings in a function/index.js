127ed563cd2a9fbbeb611eb8ef518471435f0e4a
Yorsua Alfonso <227312215+StackShadow-tech@users.noreply.github.com>
2026-05-02 15:09:13 -0400

    challenge completed

--- 03. JavaScript Fundamentals/01. Build a Counter App/29. JavaScript challenges/02. Concatenate two strings in a function/index.js
+++ 03. JavaScript Fundamentals/01. Build a Counter App/29. JavaScript challenges/02. Concatenate two strings in a function/index.js
@@ -3,3 +3,9 @@ let greeting = "Hi there"
 
 // Create a function that logs out "Hi there, Linda!" when called
 
+function greetMsg() {
+    let logMsg = greeting + ", " + name + "!"
+    console.log(logMsg)
+}
+
+greetMsg()
