41fce2092a87fa2ce9cb11f6d4ddf84a5065ca61
Yorsua Alfonso <227312215+StackShadow-tech@users.noreply.github.com>
2026-05-02 19:43:05 -0400

    html & javascript challenge completed

--- 03. JavaScript Fundamentals/01. Build a Counter App/29. JavaScript challenges/05. Rendering an error message/index.js
+++ 03. JavaScript Fundamentals/01. Build a Counter App/29. JavaScript challenges/05. Rendering an error message/index.js
@@ -2,4 +2,9 @@
 // "Something went wrong, please try again" in the paragraph
 // that has the id="error".
 
+let error = document.getElementById("error")
 
+function errorLogMsg() {
+    let errorInfo = "Something went wrong, please try again"
+    error.textContent = errorInfo
+}
