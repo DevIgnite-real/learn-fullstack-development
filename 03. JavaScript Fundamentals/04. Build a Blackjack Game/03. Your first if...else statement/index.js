788caad794aa571adb95bb79fc0afb0f2d71b65d
Yorsua Alfonso <227312215+StackShadow-tech@users.noreply.github.com>
2026-05-04 22:46:25 -0400

    lesson completed

--- 03. JavaScript Fundamentals/04. Build a Blackjack Game/03. Your first if...else statement/index.js
+++ 03. JavaScript Fundamentals/04. Build a Blackjack Game/03. Your first if...else statement/index.js
@@ -3,6 +3,18 @@
 
 let age = 22
 
+let firstCard = 10
+let secondCard = 7
+let sum = firstCard + secondCard + 6
+
+if (sum < 21) {
+    console.log("Do you want to draw a new card?")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack!")
+} else {
+    console.log("You're out of the game!")
+} 
+
 // if less than 21 -> "You can not enter the club!"
 // else            -> "Welcome!"
 
