34e3740c1e114044a844ee3a379f17ae469e8535
Yorsua Alfonso <227312215+StackShadow-tech@users.noreply.github.com>
2026-05-10 21:08:28 -0400

    first time joining html, css, and javascript by myself challenge completed

--- 03. JavaScript Fundamentals/04. Build a Blackjack Game/13. Make the start button work/index.js
+++ 03. JavaScript Fundamentals/04. Build a Blackjack Game/13. Make the start button work/index.js
@@ -8,13 +8,28 @@ let message = ""
 // 2. Create a startGame() function. Move the conditional
 // below (line 11-20) inside the body of the function.
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false
-}
-console.log(message)
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) { 
+        message = "Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+    console.log(`Still in?: ${isAlive}`)
+    console.log(`BlackJack?: ${hasBlackJack}`)
+    console.log(message)
+} 
+
+// if (sum <= 20) {
+//     message = "Do you want to draw a new card? 🙂"
+// } else if (sum === 21) {
+//     message = "Wohoo! You've got Blackjack! 🥳"
+//     hasBlackJack = true
+// } else {
+//     message = "You're out of the game! 😭"
+//     isAlive = false
+// }
+// console.log(message)
