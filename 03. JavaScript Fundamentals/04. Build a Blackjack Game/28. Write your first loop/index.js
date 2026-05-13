b8bd0f8eb3618932f62bfd3699235cb29537649f
Yorsua Alfonso <227312215+StackShadow-tech@users.noreply.github.com>
2026-05-13 16:15:21 -0400

    learned how a array based loop works

--- 03. JavaScript Fundamentals/04. Build a Blackjack Game/28. Write your first loop/index.js
+++ 03. JavaScript Fundamentals/04. Build a Blackjack Game/28. Write your first loop/index.js
@@ -5,6 +5,19 @@
 // Create a for loop that counts from 10 to 100 in steps of 10
 // Use console.log to log out the numbers
 
-for (let i = 10; i < 101; i += 10) {
-    console.log(i)
+// for (let i = 10; i < 101; i += 10) {
+//     console.log(i)
+// }
+
+let messages = [
+    "Hey, how's it going?",
+    "I'm great, thank you! How about you?", 
+    "All good. Been working on my portfolio lately.",
+    "Same here!",
+    "Great to hear",
+    "Hi"
+]
+
+for (let i = 0; i < messages.length; i += 1) { 
+    console.log(messages[i])
 }
