de75d6d0da91bf413942facf70af5c346d17587c
Yorsua Alfonso <227312215+StackShadow-tech@users.noreply.github.com>
2026-05-12 20:36:09 -0400

    challenge completed

--- 03. JavaScript Fundamentals/04. Build a Blackjack Game/23. Arrays with multiple data types/index.js
+++ 03. JavaScript Fundamentals/04. Build a Blackjack Game/23. Arrays with multiple data types/index.js
@@ -3,4 +3,10 @@
 // Create an array that describes yourself. Use the three primitive data types you've learned
 // It should contain your name (string), your age (number), and whether you like pizza (boolean)
 
+let myData = [
+    "DevIgnite Myth",
+    18,
+    true
+]
 
+console.log(myData)
