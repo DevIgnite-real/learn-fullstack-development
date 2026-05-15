e64d64343db331873c3a90067ff3bb742a72af38
Yorsua Alfonso <227312215+StackShadow-tech@users.noreply.github.com>
2026-05-15 15:03:21 -0400

    challenge completed

--- 03. JavaScript Fundamentals/04. Build a Blackjack Game/51. Create your first object/index.js
+++ 03. JavaScript Fundamentals/04. Build a Blackjack Game/51. Create your first object/index.js
@@ -2,5 +2,29 @@
 // It should contain at least one boolean, one string, one number, and one array
 // Log out at least two of the keys using the dot notation
 
+let storybookCastle = {
+    available: true,
+    host: "Pagan",
+    costPerNight: 177,
+    customerSatisfaction: [4.99, "Guest favorite", "615 Reviews"]
+}
+
+console.log(storybookCastle.available)
+console.log(storybookCastle.customerSatisfaction)
 
 
+
+
+// let course = {
+//     title: "Learn CSS Grid for free",
+//     lesson: 16,
+//     creator: "Per Harald Borgen",
+//     length: 63,
+//     level: 2,
+//     isFree: true,
+//     tags: ["html", "css"]
+// }
+
+// console.log(course.length)
+
+// console.log(course.tags)
