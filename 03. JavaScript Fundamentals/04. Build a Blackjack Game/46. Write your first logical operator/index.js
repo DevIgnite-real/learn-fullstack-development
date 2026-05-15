fe5760738379cb84bfc7e4fba8590e87ca3daa5e
Yorsua Alfonso <227312215+StackShadow-tech@users.noreply.github.com>
2026-05-15 14:23:39 -0400

    challenge completed

--- 03. JavaScript Fundamentals/04. Build a Blackjack Game/46. Write your first logical operator/index.js
+++ 03. JavaScript Fundamentals/04. Build a Blackjack Game/46. Write your first logical operator/index.js
@@ -4,8 +4,28 @@ let hasHintsLeft = false
 // Create an if statement that checks that both variables are false.
 // If so, run the showSolution() function
 
+if (hasSolvedChallenge === false && hasHintsLeft === false) {
+    showSolution()
+}
+
+
 function showSolution() {
     console.log("Showing the solution....")
 }
+
+
+
+
+// let hasCompletedCourse = true
+// let givesCertificate = true
+
+// if (hasCompletedCourse === true && givesCertificate === true) {
+//     generateCertificate()
+// }
+
+
+// function generateCertificate() {
+//     console.log("Generating certificate....")
+// }
 
 
