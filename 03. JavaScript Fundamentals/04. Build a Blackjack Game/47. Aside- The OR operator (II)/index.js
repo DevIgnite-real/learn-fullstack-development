f7e584f9dfcd5502ce51d925b6efd35be4cfcc82
Yorsua Alfonso <227312215+StackShadow-tech@users.noreply.github.com>
2026-05-15 14:28:46 -0400

    challenge completed

--- 03. JavaScript Fundamentals/04. Build a Blackjack Game/47. Aside- The OR operator (II)/index.js
+++ 03. JavaScript Fundamentals/04. Build a Blackjack Game/47. Aside- The OR operator (II)/index.js
@@ -1,6 +1,12 @@
 // Create two boolean variables, likesDocumentaries and likesStartups
 // Use an OR statement (||) to call recommendMovie() if either of those variables are true
 
+let likesDocumentaries = false
+let likeStartups = true
+
+if (likesDocumentaries === true || likeStartups === true) {
+    recommendMovie()
+}
 
 
 function recommendMovie() {
