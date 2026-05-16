5b9a741273c2a73328970f93ff15fb34d2a1cdd7
Yorsua Alfonso <227312215+StackShadow-tech@users.noreply.github.com>
2026-05-16 12:23:04 -0400

    challenge completed

--- 03. JavaScript Fundamentals/04. Build a Blackjack Game/56. JavaScript challenges/06. Rock papers scissors/index.js
+++ 03. JavaScript Fundamentals/04. Build a Blackjack Game/56. JavaScript challenges/06. Rock papers scissors/index.js
@@ -2,4 +2,10 @@ let hands = ["rock", "paper", "scissor"]
 
 // Create a function that returns a random item from the array
 
+function returnRandomChoice() {
+    let randomChoice = Math.floor(Math.random() * hands.length)
 
+    return hands[randomChoice]
+}
+
+console.log(returnRandomChoice())
