83b2188b0b2904cbf20d9e590a7f5dbd9af82249
Yorsua Alfonso <227312215+StackShadow-tech@users.noreply.github.com>
2026-05-16 12:36:48 -0400

    challenge completed

--- 03. JavaScript Fundamentals/04. Build a Blackjack Game/56. JavaScript challenges/07. EmojiFighter/index.js
+++ 03. JavaScript Fundamentals/04. Build a Blackjack Game/56. JavaScript challenges/07. EmojiFighter/index.js
@@ -7,4 +7,11 @@ fightButton.addEventListener("click", function() {
     // Challenge:
     // When the user clicks on the "Pick Fighters" button, pick two random 
     // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
+    let firstRandomChoice = Math.floor(Math.random() * fighters.length)
+
+    let secondRandomChoice = Math.floor(Math.random() * fighters.length)
+
+    let displayFighters = fighters[firstRandomChoice] + " VS " + fighters[secondRandomChoice]
+
+    stageEl.textContent = displayFighters
 })
