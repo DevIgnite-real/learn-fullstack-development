6975d3bb91f9854a6163fafbeb5cd9c98f93eabc
Yorsua Alfonso <227312215+StackShadow-tech@users.noreply.github.com>
2026-05-19 16:13:19 -0400

    challenge completed

--- 03. JavaScript Fundamentals/06. Build a Chrome Extension/06. Your turn to refactor/index.js
+++ 03. JavaScript Fundamentals/06. Build a Chrome Extension/06. Your turn to refactor/index.js
@@ -1,9 +1,11 @@
 // Refactor the code so that it uses .addEventListener()
 // when you click the SAVE INPUT button
 
-function saveLead() {
+let inputBtnEl = document.getElementById("input-btn")
+
+inputBtnEl.addEventListener("click", function() {
     console.log("Button clicked!")
-}
+})
 
 
 
