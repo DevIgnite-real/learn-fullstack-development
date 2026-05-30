ad1d2fb6fdca3c72d8bfee601ded140884b781e0
Yorsua Alfonso <227312215+StackShadow-tech@users.noreply.github.com>
2026-05-30 18:59:04 -0400

    challenge completed

--- 03. JavaScript Fundamentals/06. Build a Chrome Extension/56. JavaScript challenges - part 3/06. Render images/index.js
+++ 03. JavaScript Fundamentals/06. Build a Chrome Extension/56. JavaScript challenges - part 3/06. Render images/index.js
@@ -2,9 +2,23 @@
 // Use a for loop, template strings (``), plus equals (+=)
 // .innerHTML to solve the challenge.
 
+const container = document.getElementById("container")
+
 const imgs = [
     "images/hip1.jpg",
     "images/hip2.jpg",
     "images/hip3.jpg"
 ]
 
+function renderImages(containerEL) {
+    let storeImages = ``
+
+    for (let i = 0; i < imgs.length; i++) {
+        storeImages += `
+            <img alt="Employee in the company" class="team-img" src="${imgs[i]}">
+        `
+    }
+    containerEL.innerHTML = storeImages
+}
+
+renderImages(container)
