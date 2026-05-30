7e3134f9d280778bd6aeb9b55f0d433ecb428367
Yorsua Alfonso <227312215+StackShadow-tech@users.noreply.github.com>
2026-05-30 17:06:46 -0400

    challenge completed

--- 03. JavaScript Fundamentals/06. Build a Chrome Extension/56. JavaScript challenges - part 3/03. save to localStorage/index.js
+++ 03. JavaScript Fundamentals/06. Build a Chrome Extension/56. JavaScript challenges - part 3/03. save to localStorage/index.js
@@ -1,3 +1,9 @@
 // Save a value to localStorage
 // Delete your code and refresh the page
 // Fetch your value from localStorage and log it out
+
+// const password = "123456AI"
+
+// localStorage.setItem("password", JSON.stringify(password))
+
+console.log(localStorage.getItem("password"))
